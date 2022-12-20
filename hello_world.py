a2d95a3205de3d295ecf451ac96dc91da66cf616
mononokay <mononokay@proton.me>
2022-12-19 20:05:11 -0500

    a joke, for laughs

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+# print("Hello World!")
+
+# message = "hello python world!"
+# print(message)
+# print(message.title())
+# print(message.upper())
+
+first_name = "psuedo"
+last_name = "intellectual"
+full_name = f"{first_name} {last_name}"
+# f-strings are used for concatenation
+print(full_name.title())
