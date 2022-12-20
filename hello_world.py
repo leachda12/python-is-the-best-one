96ab99e1fc14e4d5511a31773b9ad12b4c7a9553
mononokay <mononokay@proton.me>
2022-12-19 20:37:26 -0500

    scooting through the easy bits

--- hello_world.py
+++ hello_world.py
@@ -10,3 +10,21 @@ last_name = "intellectual"
 full_name = f"{first_name} {last_name}"
 # f-strings are used for concatenation
 print(full_name.title())
+message = f"I can't stand {full_name}s!"
+print(message)
+
+
+# # lstrip removes left whitespace, rstrip removes right whitespace
+# word = " python "
+# print(word.lstrip())
+# print(word.rstrip())
+# print(word.lstrip().rstrip())
+# # jk that one is easier
+# print(word.strip)
+
+
+# use caps to indicate that a variable shsould be treated as a constant
+AH_YES = "that's nice"
+
+# ye olden zen of python
+import this
